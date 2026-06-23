702f9ecfcf92152f598c9957dfbf3cfb0745a56c
peixinss <renan.a.peixinho@gmail.com>
2026-06-22 21:54:09 -0300

    super atualização apolo

--- script.js
+++ script.js
@@ -4,11 +4,6 @@
             miniatura.classList.add('ativa');
         }
 
-        function selecionarCor(el) {
-            document.querySelectorAll('.cor').forEach(c => c.classList.remove('ativa'));
-            el.classList.add('ativa');
-        }
-
         function adicionarCarrinho() {
             const toast = document.getElementById('toast');
             toast.classList.add('show');
